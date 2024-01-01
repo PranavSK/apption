9f82654c703eb57116aab574ec0731538c156f2a
Pranav Koundinya <pranavpg101@gmail.com>
2024-01-01 23:35:36 +0530

    feat(db): add table for documents (#7)
    Added schema tables for documents, which support nesting and trash. Closes #6
    
    complete #6

--- db/schema/public.ts
+++ db/schema/public.ts
@@ -1,4 +1,13 @@
-import { pgTable, text, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
+import {
+  boolean,
+  foreignKey,
+  pgTable,
+  text,
+  timestamp,
+  unique,
+  uuid,
+  varchar,
+} from 'drizzle-orm/pg-core';
 
 import { user } from './auth';
 
@@ -15,12 +24,42 @@ export const profile = pgTable('user_profile', {
   website: text('website'),
 });
 
-export const workspace = pgTable('workspace', {
-  id: uuid('id').defaultRandom().primaryKey().notNull(),
-  createdAt: timestamp('created_at', { withTimezone: true, mode: 'string' }).defaultNow().notNull(),
-  owner: varchar('owner', { length: 15 })
-    .notNull()
-    .references(() => user.id, { onDelete: 'cascade' }),
-  name: text('name').notNull(),
-  icon: text('icon').notNull(),
-});
+export const workspace = pgTable(
+  'workspace',
+  {
+    id: uuid('id').defaultRandom().primaryKey().notNull(),
+    createdAt: timestamp('created_at', { withTimezone: true, mode: 'date' }).defaultNow().notNull(),
+    updatedAt: timestamp('updated_at', { withTimezone: true, mode: 'date' }).defaultNow().notNull(),
+    owner: varchar('owner', { length: 15 })
+      .references(() => user.id, { onDelete: 'cascade' })
+      .notNull(),
+    name: text('name').notNull(),
+    icon: text('icon').notNull(),
+    inTrash: boolean('in_trash').default(false),
+  },
+  (table) => ({
+    uniqueForUser: unique().on(table.owner, table.name),
+  }),
+);
+
+export const document = pgTable(
+  'document',
+  {
+    id: uuid('id').defaultRandom().primaryKey().notNull(),
+    createdAt: timestamp('created_at', { withTimezone: true, mode: 'date' }).defaultNow().notNull(),
+    updatedAt: timestamp('updated_at', { withTimezone: true, mode: 'date' }).defaultNow().notNull(),
+    workspaceId: uuid('workspace_id')
+      .references(() => workspace.id, { onDelete: 'cascade' })
+      .notNull(),
+    // Using foreignKey helper since typescript doesn't identify self references
+    parentId: uuid('parent_id'),
+    name: text('name').notNull(),
+    content: text('content'),
+    bannerUrl: text('banner_url'),
+    inTrash: boolean('in_trash').default(false),
+  },
+  (table) => ({
+    uniqueForWorkspace: unique().on(table.workspaceId, table.name),
+    parentReference: foreignKey({ columns: [table.parentId], foreignColumns: [table.id] }),
+  }),
+);
